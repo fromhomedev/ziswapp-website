4fbe7c484456133f193c4850d3ae6e346de0c5d5
Nuradiyana <me@nooradiana.com>
2022-07-09 04:52:03 +0700

    Add umami

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 
 import { theme as chakraTheme } from "@chakra-ui/pro-theme";
 import "@fontsource/inter/variable.css";
+import Script from "next/script";
 
 const theme = extendTheme(
   {
@@ -14,6 +15,12 @@ const theme = extendTheme(
 function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Script
+        async
+        defer
+        data-website-id="8ae2e4c7-ba23-4116-952b-eddebe53b9b0"
+        src="https://umami.fromhome.dev/umami.js"
+      />
       <Component {...pageProps} />
     </ChakraProvider>
   );
